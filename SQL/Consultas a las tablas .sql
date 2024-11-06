01bf28999db8b0e49e3e8de8e9b54108b96ae9ec
milageroscancela <milagroscancela@Milagross-MacBook-Pro.local>
2024-11-06 11:38:16 -0300

    consultas sql

--- SQL/Consultas a las tablas .sql
+++ SQL/Consultas a las tablas .sql
@@ -4,27 +4,21 @@ USE EscuelaNieve;
 SELECT
     a.descripcion AS nombre_actividad,
     SUM(a.costo + e.costo) AS ingresos_totales
-FROM
-    actividades a JOIN clase c ON a.id = c.id_actividad
+FROM actividades a JOIN clase c ON a.id = c.id_actividad
                   JOIN alumno_clase ac ON c.id = ac.id_clase
                   JOIN equipamiento e ON ac.id_equipamiento = e.id
-GROUP BY
-    a.descripcion
-ORDER BY
-    ingresos_totales DESC;
+GROUP BY a.descripcion
+ORDER BY ingresos_totales DESC;
 
 # Actividades con más alumnos
 
 SELECT
     a.descripcion AS nombre_actividad,
     COUNT(ac.ci_alumno) AS cantidad_alumnos
-FROM
-    actividades a JOIN clase c ON a.id = c.id_actividad
+FROM actividades a JOIN clase c ON a.id = c.id_actividad
                   JOIN alumno_clase ac ON c.id = ac.id_clase
-GROUP BY
-    a.descripcion
-ORDER BY
-    cantidad_alumnos DESC;
+GROUP BY a.descripcion
+ORDER BY cantidad_alumnos DESC;
 
 # Los turnos con más clases dictadas
 SELECT a.descripcion AS nombre_actividad,
